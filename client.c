373ad1959652207b9978d20d26c83797075ab268
Daniel Morales Miguel <daniel.morales.miguel@gmail.com>
2017-10-20 19:53:55 -0300

    Presend validation

--- client.c
+++ client.c
@@ -1,8 +1,154 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define PORT "3490" // the port client will be connecting to 
+#define MAXDATASIZE 100 // max number of bytes we can get at once 
+#define MAX_SIZE 1024
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+  if (sa->sa_family == AF_INET) {
+    return &(((struct sockaddr_in*)sa)->sin_addr);
+  }
+
+  return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
 
 int main(int argc, char const *argv[])
 {
-  /* code */
+  int i, j, k, l, bsn_counter, phrase_size[3], triplets;
+  int valid, correct;
+  char helper;
+  scanf("%d\n", &triplets);
+
+  char *triplet[3];
+
+  for(i = 0; i < triplets; i++){
+    triplet[0] = (char*) malloc(sizeof(char)*MAX_SIZE);
+    triplet[1] = (char*) malloc(sizeof(char)*MAX_SIZE);
+    triplet[2] = (char*) malloc(sizeof(char)*MAX_SIZE);
+
+    bsn_counter = 0;
+    j = 0;
+
+    while(bsn_counter < 3){
+      if (j == MAX_SIZE){
+        realloc(triplet[bsn_counter], MAX_SIZE*sizeof(char));
+      }
+
+      helper = getchar();
+      
+      if (helper == '\n' || helper == EOF){
+        phrase_size[bsn_counter] = j;
+        bsn_counter++;
+        j = 0;
+      } else {
+        triplet[bsn_counter][j] = helper;
+        j++;
+      }
+    }
+
+    /* Check if coded phrase exists */
+    valid = 0;
+    for (l = 0; l < 3; l++){
+      if (phrase_size[l] == 43){
+        if (triplet[l][3] == ' ' && triplet[l][9] == ' ' && triplet[l][15] == ' ' && triplet[l][19] == ' ' && triplet[l][25] == ' ' && triplet[l][30] == ' ' && triplet[l][34] == ' ' && triplet[l][39] == ' '){
+          valid = 1;
+          correct = l;
+          break;
+        }
+      }
+    }
+    
+    if (valid == 0){
+      printf("Invalid message for decoding!!");
+      return -1;
+    } 
+
+    /* Send to Server */
+    int sockfd, numbytes;  
+    char codification[26];
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
+
+    if (argc != 2) {
+        fprintf(stderr,"usage: client hostname\n");
+        exit(1);
+    }
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+      fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+      return 1;
+    }
+
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+      if ((sockfd = socket(p->ai_family, p->ai_socktype,
+          p->ai_protocol)) == -1) {
+        perror("client: socket");
+        continue;
+      }
+
+      if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+        perror("client: connect");
+        close(sockfd);
+        continue;
+      }
+
+      break;
+    }
+
+    if (p == NULL) {
+      fprintf(stderr, "client: failed to connect\n");
+      return 2;
+    }
+
+    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+        s, sizeof s);
+    printf("client: connecting to %s\n", s);
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    printf("Enviar: %s\n", triplet[correct]);
+    send(sockfd, &triplet[correct], 43, 0);
+
+    /* Receive Codification from Server */
+    if ((numbytes = recv(sockfd, codification, 26, 0)) == -1) {
+        perror("recv");
+        exit(1);
+    }
+    close(sockfd);
+
+    /* Print Decodification */
+    for (l = 0; l < 3; l++){
+      for (k = 0; k < phrase_size[l]; k++){
+        if (triplet[l][k] == ' '){
+          printf(" ");
+        } else {
+          printf("%c", codification[triplet[l][k]-'a']);
+        }
+      }
+      printf("\n");
+    }
+
+    /* Free Memory Allocation for Strings */
+    free(triplet[0]);
+    free(triplet[1]);
+    free(triplet[2]);
+  }
+
   return 0;
 }
--- server.c
+++ server.c
@@ -1,104 +1,181 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
 
-#define MAX_SIZE 1024
+#define PORT "3490"  // the port users will be connecting to
+#define BACKLOG 10   // how many pending connections queue will hold
 
-char *find_codification(char *input, int size){
-  if (size == 43){
-    // the3quick9brown15fox19jumps25over30the34lazy39dog
-    if (input[3] == ' ' && input[9] == ' ' && input[15] == ' ' && input[19] == ' ' && input[25] == ' ' && input[30] == ' ' && input[34] == ' ' && input[39] == ' '){
-      char *output = (char *) malloc(sizeof(char)*26);
+void sigchld_handler(int s)
+{
+  // waitpid() might overwrite errno, so we save and restore it:
+  int saved_errno = errno;
 
-      output[input[36]-'a'] = 'a';
-      output[input[10]-'a'] = 'b';
-      output[input[7]-'a'] = 'c';
-      output[input[40]-'a'] = 'd';
-      output[input[2]-'a'] = 'e';
-      output[input[16]-'a'] = 'f';
-      output[input[42]-'a'] = 'g';
-      output[input[1]-'a'] = 'h';
-      output[input[6]-'a'] = 'i';
-      output[input[20]-'a'] = 'j';
-      output[input[8]-'a'] = 'k';
-      output[input[35]-'a'] = 'l';
-      output[input[22]-'a'] = 'm';
-      output[input[14]-'a'] = 'n';
-      output[input[12]-'a'] = 'o';
-      output[input[23]-'a'] = 'p';
-      output[input[4]-'a'] = 'q';
-      output[input[11]-'a'] = 'r';
-      output[input[24]-'a'] = 's';
-      output[input[0]-'a'] = 't';
-      output[input[5]-'a'] = 'u';
-      output[input[27]-'a'] = 'v';
-      output[input[13]-'a'] = 'w';
-      output[input[18]-'a'] = 'x';
-      output[input[38]-'a'] = 'y';
-      output[input[37]-'a'] = 'z';
+  while(waitpid(-1, NULL, WNOHANG) > 0);
 
-      return output;
-    }
+  errno = saved_errno;
+}
+
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+  if (sa->sa_family == AF_INET) {
+    return &(((struct sockaddr_in*)sa)->sin_addr);
   }
-  return NULL;
+
+  return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+char *find_codification(char *input){
+  char *output = (char *) malloc(sizeof(char)*26);
+
+  output[input[36]-'a'] = 'a';
+  output[input[10]-'a'] = 'b';
+  output[input[7]-'a'] = 'c';
+  output[input[40]-'a'] = 'd';
+  output[input[2]-'a'] = 'e';
+  output[input[16]-'a'] = 'f';
+  output[input[42]-'a'] = 'g';
+  output[input[1]-'a'] = 'h';
+  output[input[6]-'a'] = 'i';
+  output[input[20]-'a'] = 'j';
+  output[input[8]-'a'] = 'k';
+  output[input[35]-'a'] = 'l';
+  output[input[22]-'a'] = 'm';
+  output[input[14]-'a'] = 'n';
+  output[input[12]-'a'] = 'o';
+  output[input[23]-'a'] = 'p';
+  output[input[4]-'a'] = 'q';
+  output[input[11]-'a'] = 'r';
+  output[input[24]-'a'] = 's';
+  output[input[0]-'a'] = 't';
+  output[input[5]-'a'] = 'u';
+  output[input[27]-'a'] = 'v';
+  output[input[13]-'a'] = 'w';
+  output[input[18]-'a'] = 'x';
+  output[input[38]-'a'] = 'y';
+  output[input[37]-'a'] = 'z';
+
+  return output;
 }
 
 int main(int argc, char const *argv[])
 {
-  int i, j, k, l, bsn_counter, phrase_size[3], triplets;
-  char helper;
-  scanf("%d\n", &triplets);
+  /* Set-up socket connection */
+  int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+  struct addrinfo hints, *servinfo, *p;
+  struct sockaddr_storage their_addr; // connector's address information
+  socklen_t sin_size;
+  struct sigaction sa;
+  int yes=1;
+  char s[INET6_ADDRSTRLEN];
+  int rv;
+  char *codification = NULL;
+  char input[43];
 
-  char *triplet[3];
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE; // use my IP
 
-  for(i = 0; i < triplets; i++){
-    triplet[0] = (char*) malloc(sizeof(char)*MAX_SIZE);
-    triplet[1] = (char*) malloc(sizeof(char)*MAX_SIZE);
-    triplet[2] = (char*) malloc(sizeof(char)*MAX_SIZE);
+  if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
+  }
 
-    bsn_counter = 0;
-    j = 0;
+  // loop through all the results and bind to the first we can
+  for(p = servinfo; p != NULL; p = p->ai_next) {
+    if ((sockfd = socket(p->ai_family, p->ai_socktype,
+        p->ai_protocol)) == -1) {
+      perror("server: socket");
+      continue;
+    }
 
-    while(bsn_counter < 3){
-      if (j == MAX_SIZE){
-        realloc(triplet[bsn_counter], MAX_SIZE*sizeof(char));
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+        sizeof(int)) == -1) {
+      perror("setsockopt");
+      exit(1);
+    }
+
+    if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+      close(sockfd);
+      perror("server: bind");
+      continue;
+    }
+
+    break;
+  }
+
+  freeaddrinfo(servinfo); // all done with this structure
+
+  if (p == NULL)  {
+    fprintf(stderr, "server: failed to bind\n");
+    exit(1);
+  }
+
+  if (listen(sockfd, BACKLOG) == -1) {
+    perror("listen");
+    exit(1);
+  }
+
+  sa.sa_handler = sigchld_handler; // reap all dead processes
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+  if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+    perror("sigaction");
+    exit(1);
+  }
+
+  printf("server: waiting for connections...\n");
+
+  int size;
+
+  while(1)
+  {
+    /* Receive communication from client */
+    sin_size = sizeof their_addr;
+    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+    if (new_fd == -1) {
+      perror("accept");
+      continue;
+    }
+
+    /* Begin receiving string with encoded phrase */
+    inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
+    printf("server: got connection from %s\n", s);
+
+    size = recv(sockfd, input, 43, 0);
+    printf("Input: %s, size: %d\n", input, size);
+
+    if (!fork()) {
+      close(sockfd);
+
+      /* Process codification */
+      codification = find_codification(input);
+
+      /* Send codification to client */
+      if (send(new_fd, codification, 26, 0) == -1)
+      {
+        perror("send");
       }
-
-      helper = getchar();
       
-      if (helper == '\n' || helper == EOF){
-        phrase_size[bsn_counter] = j;
-        bsn_counter++;
-        j = 0;
-      } else {
-        triplet[bsn_counter][j] = helper;
-        j++;
-      }
-    }
+      close(new_fd);
 
-    char *codification = NULL;
-    /* check if base string in first phrase */
-    codification = find_codification(triplet[0], phrase_size[0]);
-    if (codification == NULL){
-      codification = find_codification(triplet[1], phrase_size[1]);
-      if (codification == NULL){
-        codification = find_codification(triplet[2], phrase_size[2]);
-      }
+      exit(0);
     }
+    close(new_fd);
 
-    for (l = 0; l < 3; l++){
-      for (k = 0; k < phrase_size[l]; k++){
-        if (triplet[l][k] == ' '){
-          printf(" ");
-        } else {
-          printf("%c", codification[triplet[l][k]-'a']);
-        }
-      }
-      printf("\n");
-    }
-
-    free(triplet[0]);
-    free(triplet[1]);
-    free(triplet[2]);
+    /* Process codification */
+    codification = find_codification(input);
   }
 
   return 0;
